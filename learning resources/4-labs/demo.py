8a085acfc87b8d594155284901415b4e738ab502
linksdl <794469928@qq.com>
2020-10-23 14:02:58 +0100

    add files to weeks4

---
+++ learning resources/4-labs/demo.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Project : acs-project-machine_learning      
+@File    : demo.py
+@Author  : Billy Sheng 
+@Contact : shengdl999links@gmail.com  
+@Date    : 2020/10/22 8:34 下午
+@Version  : 1.0.0
+@License : Apache License 2.0
+@Desc    : No
+"""
+
+from sklearn.linear_model import SGDRegressor
+
+SGDRegressor.fit()
